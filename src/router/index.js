cb75764d8ebdc76fb7321d9d5db975d1858ccabd
geyaoAlone <gy565581868@126.com>
2018-06-06 11:19:09 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import {post,fetch,patch,put} from '@/comm/common'
-import {delCookie,getCookie} from '@/comm/cookieUtil'
-
 import Login from '../pages/login'
 import FirstPage from '../pages/firstPage'
 Vue.use(Router)
 
-
-Vue.prototype.$post = post;
 const routes = [
   {
     path: '/',
